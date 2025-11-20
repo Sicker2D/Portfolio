857439ef0adfe6fe5c1c23114bb56ac7a290cbdd
Sicker2D <98846475+Sicker2D@users.noreply.github.com>
2025-11-19 23:48:37 -0600

    Add files via upload

---
+++ Simple Chatbot.py
@@ -0,0 +1,13 @@
+bot_name: str = 'Bob'
+print(f'Hello! My name is {bot_name}!')
+
+print(f'What is your name?')
+user_name: str = input()
+print(f'Nice to meet you, {user_name}!')
+print('How old are you?')
+user_age: int = int(input())
+print(f'You are {user_age} years old! That\'s great!')
+print('What is your favorite programming language?')
+user_language: str = input()
+print(f'{user_language} is a wonderful language!')
+print('It was nice chatting with you! Goodbye!')
